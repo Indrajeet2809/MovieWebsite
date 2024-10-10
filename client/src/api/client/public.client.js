24e9fd48ba79d34b7ea804d2374574ea11e20592
Indrajeet2809 <indrajeetraj2809@gmail.com>
2024-10-10 11:45:26 +0530

    Initial commit

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import queryString from "query-string";
 
 const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+// const baseURL = "http://localhost:5000/";
 
 const publicClient = axios.create({
   baseURL,
